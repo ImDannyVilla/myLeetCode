a894425e4a583cc0c5b8f53f9a7ccb24b6526e3a
ImDannyVilla <157629943+ImDannyVilla@users.noreply.github.com>
2025-11-18 10:20:24 -0800

    0217-CONTAINS-DUPLICATE Stats: Time: 23 ms (86.01%), Space: 71 MB (78.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
         int n = nums.size();
- 
-        for(int num : nums){
-            if(seen.count(num) > 0){
+        sort(nums.begin(), nums.end());
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]){
                 return true;
             }
-            seen.insert(num);
-        }
-        return false;
+        }    
+        return false;    
     }
 };

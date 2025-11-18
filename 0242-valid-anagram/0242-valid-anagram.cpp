a90866ce58d29902605031ff739d8289f3a13306
ImDannyVilla <157629943+ImDannyVilla@users.noreply.github.com>
2025-11-18 10:46:17 -0800

    0242-VALID-ANAGRAM Stats: Time: 7 ms (20.54%), Space: 9.7 MB (47.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -8,12 +8,7 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] != t[i]){
-                return false;
-            }
-        }
-        return true;
+        return t == s;
         
     }
 };
